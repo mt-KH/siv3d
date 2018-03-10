e3d7494be56a0582fcb5dd905dc9f9ba833ac772
kooooo <bg.hachi98@gmail.com>
2018-03-10 23:29:10 +0900

    プリミティブ型のタイプの追加

--- First/First/Essence/Header/Common/Primitive/Base/IPrimitive.h
+++ First/First/Essence/Header/Common/Primitive/Base/IPrimitive.h
@@ -4,10 +4,24 @@
 namespace essence {
 	namespace primitve{
 
+/// プリミティブタイプの列挙
+enum class EType {
+	Invalid = -1,  /// 不正な型
+	Int32,
+	Int64,
+	Float32,
+	Float64,
+	String,
+	Bool,
+	Bit
+};
+
 class IPrimitive {
 
 public:
 	virtual ~IPrimitive() = default;
+
+	virtual EType Type() = 0;
 };
 
 }}
